9501ada40188d29c3afd5c14d96e64f0846d4326
Eivind Lysne <eivindlysne@gmail.com>
2016-01-25 21:47:18 +0100

    Chanes to OpenGL context

--- src/main/java/me/lysne/Main.java
+++ src/main/java/me/lysne/Main.java
@@ -52,7 +52,7 @@ public class Main {
 
         font = new Font(Config.FONT_DIR + "signed.fnt");
         testText = new TextMesh("FPS: 00", font, 2, new Vector2f(0, Config.WINDOW_HEIGHT), 12, true)
-                .color(1f, 1f, 0f).build();
+                .color(0f, 0f, 0f).build();
     }
 
     private void destroy() {
--- src/main/java/me/lysne/window/Display.java
+++ src/main/java/me/lysne/window/Display.java
@@ -43,8 +43,8 @@ public class Display {
         glfwWindowHint(GLFW_VISIBLE, GL11.GL_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GL11.GL_FALSE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL11.GL_TRUE);
 
         handle = glfwCreateWindow(width, height, title, NULL, NULL);
